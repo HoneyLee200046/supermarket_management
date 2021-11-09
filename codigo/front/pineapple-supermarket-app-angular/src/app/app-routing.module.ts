f5401d3c9c0d96391617d9476e084b2a9df572e8
hackerrank <me@hackerrank.com>
2021-11-08 18:36:43 -0600

    Se agrega funcionalidad al boton de Iniciar Sesion para que muestre el componente index

--- codigo/front/pineapple-supermarket-app-angular/src/app/app-routing.module.ts
+++ codigo/front/pineapple-supermarket-app-angular/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UsuariosComponent } from './components/usuarios/usuarios.component';
+import { IndexComponent } from './components/index/index.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
   { path: 'usuarios', component: UsuariosComponent},
+  { path: 'index', component: IndexComponent},
   { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
 
--- codigo/front/pineapple-supermarket-app-angular/src/app/components/usuarios/usuarios.component.ts
+++ codigo/front/pineapple-supermarket-app-angular/src/app/components/usuarios/usuarios.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-usuarios',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuariosComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _router:Router) {  
+  }
 
   ngOnInit(): void {
   }
 
+  entrar() {
+    this._router.navigate(['/index']);
+  }
+
 }
+
