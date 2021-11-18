2a028e40c0231ba8409b52af8a0ff4ac9320da70
Gala Karina Reyes Peña <galakarinareyes@gmail.com>
2021-11-18 12:41:32 -0600

    Cambios en metodo para obtener el ROL

--- codigo/ws/src/main/java/com/ibm/grupo2/service/RolesImp.java
+++ codigo/ws/src/main/java/com/ibm/grupo2/service/RolesImp.java
@@ -1,6 +1,9 @@
 package com.ibm.grupo2.service;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import com.ibm.grupo2.dto.seguridad.GrupoDTO;
 
 import com.ibm.grupo2.model.seguridad.Grupo;
 import com.ibm.grupo2.repository.usuarios.RolesRepo;
@@ -13,10 +16,28 @@ public class RolesImp implements RolesService{
 
     @Autowired
     private RolesRepo rolesRepo;
-
+  
     @Override
-    public List<Grupo> getRoles(){
-        return (List<Grupo>) rolesRepo.findAll();
+    public List<GrupoDTO> getRoles(){
+        List<Grupo> listabd =  (List<Grupo>) rolesRepo.findAll();
+        List<GrupoDTO> resultado = new ArrayList<>();
+        listabd.forEach((grupo)-> {
+            resultado.add(crearGrupoDTO(grupo));
+        });
 
+        return resultado;
     }
+
+    private GrupoDTO crearGrupoDTO(Grupo grupoBD) {
+        GrupoDTO respuesta = new GrupoDTO();
+
+        respuesta.setIdGrupo(grupoBD.getIdGrupo());
+        respuesta.setNombreGrupo(grupoBD.getNombreGrupo());
+        respuesta.setEstatusGrupo(grupoBD.getEstatusGrupo());
+      
+        return respuesta;
+    }
+
+    
+
 }
