c861aca6061e3685456f2226f07fdb23ae8085de
LENOVO\Gala Karina <galakarinareyes@gmail.com>
2021-11-18 08:44:21 -0600

    @service en rolesImp

--- codigo/ws/src/main/java/com/ibm/grupo2/service/RolesImp.java
+++ codigo/ws/src/main/java/com/ibm/grupo2/service/RolesImp.java
@@ -6,7 +6,8 @@ import com.ibm.grupo2.model.seguridad.Grupo;
 import com.ibm.grupo2.repository.usuarios.RolesRepo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.stereotype.Service;
+@Service
 public class RolesImp implements RolesService{
     
 
