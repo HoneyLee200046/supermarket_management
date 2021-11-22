a93916fae830857ee8eb95eaefeb0e47f7a565dd
LENOVO\Gala Karina <galakarinareyes@gmail.com>
2021-11-21 23:10:53 -0600

    validaciones nuevo producto

--- codigo/front/pineapple-supermarket-app-angular/src/app/components/productos/productos.component.ts
+++ codigo/front/pineapple-supermarket-app-angular/src/app/components/productos/productos.component.ts
@@ -10,14 +10,21 @@ import { ProductoService } from 'src/app/services/producto-service';
 export class ProductosComponent implements OnInit {
 
   listaProductos:Producto[] = [];
+  public cargando : boolean = true;
 
   constructor(private _productoService:ProductoService) { }
 
   ngOnInit(): void {
+    this.cargarProductos();   
+  }
+
+  cargarProductos(){
+    this.cargando= true;
 
     this._productoService.getListaProductos()
     .subscribe((data:any[]) => {
       this.listaProductos=data;
+      this.cargando = false;
     });
   }
 
